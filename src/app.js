232f9dc18dda97f301fbd5772e82aa9c6fd01303
pansay <jan.krepelka@gmail.com>
2017-07-09 18:49:30 +0200

    clean up

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ angular.module('frenchify', [])
     .constant('texts', require('../texts.json'))
     .service('showdown', [require('showdown').Converter])
     .constant('frenchify', require('./frenchify'))
-    .constant('rules', require('../rules.json'))
+    .constant('rules', require('../rules/rules.json'))
     .constant('rules_langs', {
-        fr: require('../rules-fr.json'),
-        en: require('../rules-en.json')
+        fr: require('../rules/rules-fr.json'),
+        en: require('../rules/rules-en.json')
     })
     .controller('MainController', require('./MainController'));
